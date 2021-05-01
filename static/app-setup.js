88565565fc2f33a8cbc0a833883bd43f9064c2d6
Alex Meade Wilson <alex2.wilson@gmail.com>
2021-05-01 17:20:42 +0100

    2nd Commit - Deployment Pipeline working with Firebase Auth and Datastore. Used Ex06 code.

---
+++ static/app-setup.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyBawwAbMVDvHt4yewL5cdBNpje-_I2KxB8",
+	authDomain: "amw-csp-dropbox.firebaseapp.com",
+	databaseURL: "https://amw-csp-dropbox.firebaseio.com",
+	projectId: "amw-csp-dropbox",
+	storageBucket: "amw-csp-dropbox.appspot.com",
+	messagingSenderId: "535535648946"
+};
+firebase.initializeApp(firebaseConfig);
